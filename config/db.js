dcede671bd2d41f4d925f5fea5e7020e7e665c57
ASWINTK84 <tkaswin0911@gmail.com>
2025-06-11 19:43:52 +0530

    changes indb

--- config/db.js
+++ config/db.js
@@ -2,17 +2,17 @@ import mongoose from 'mongoose';
 
 
  const connectDB = async () => {
-    try {
-
-        const conn = await mongoose.connect("mongodb+srv://tkaswin0911:2xf8GiDhpfduc9Ex@cluster0.wy1h40f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-        console.log("connected to mongoDB" + " " + conn.connection.host);
-        
-
-    } catch (error) {
-        console.log('error in mongodb' + error);
-        
-    }
- }
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1);
+  }
+};
 
 
  export default connectDB;
