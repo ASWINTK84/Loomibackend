6a83c8971753222d127ef3e7f1716f0c01585edf
ASWINTK84 <tkaswin0911@gmail.com>
2025-06-11 19:20:54 +0530

    db connection chaanges

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
  const connectDB = async () => {
     try {
 
-        const conn = await mongoose.connect(process.env.MONGO_URL)
+        const conn = await mongoose.connect("mongodb+srv://tkaswin0911:2xf8GiDhpfduc9Ex@cluster0.wy1h40f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         console.log("connected to mongoDB" + " " + conn.connection.host);
         
 
